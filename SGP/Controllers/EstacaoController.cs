0d573d4f29ad53052cbe561cdfd81b9a8f04a8f1
Tiago Santos <tiagosantos.cto@gmail.com>
2020-10-03 20:29:29 -0300

    Cadastro de estação.

---
+++ SGP/Controllers/EstacaoController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SGP.Models;
+
+namespace SGP.Controllers
+{
+    public class EstacaoController : Controller
+    {
+        IHttpContextAccessor HttpContextAccessor;
+        public EstacaoController(IHttpContextAccessor httpContextAccessor) { HttpContextAccessor = httpContextAccessor; }
+
+       
+        [HttpGet]
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registrar(EstacaoModel Estacao)
+        {
+            if (ModelState.IsValid)
+            {
+                Estacao.GravarEstacao();
+                return RedirectToAction("Sucesso");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int id)
+        {
+            var Estacao = new EstacaoModel(HttpContextAccessor);
+            Estacao.ExcluirEstacao(id);
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Sucesso()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SGP/Models/EstacaoModel.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using SGP.Util;
+using System;
+using System.Collections.Generic;
+
+namespace SGP.Models
+{
+    public class EstacaoModel
+    {
+        public int  Id { get; set; }
+        public string Descricao { get; set; }
+        public string Tipo { get; set; }
+ 
+        public IHttpContextAccessor HttpContextAccessor { get; set; }
+
+        public EstacaoModel(){}
+
+        public EstacaoModel(IHttpContextAccessor httpContextAccessor)
+        {
+            HttpContextAccessor = httpContextAccessor;
+        }
+
+        public void GravarEstacao()
+        {
+            string sql;
+
+            if (Id == 0)
+            {
+                sql = $"INSERT INTO ESTACAO_EST  (DESCRICAO, TIPO) VALUES ('{Descricao}', {Tipo})";
+            }
+            else
+            {
+                sql = $"UPDATE  ESTACAO_EST SET DESCRICAO = '{Descricao}',  TIPO = '{Tipo} WHERE ID = '{Id}'";
+            }
+
+            var dal = new DAL();
+            dal.ExecutarComandoSQL(sql);
+        }
+
+        public void ExcluirEstacao(int id)
+        {
+            string sql = $"DELETE FROM ESTACAO_EST WHERE ID = {id}";
+            var dal = new DAL();
+            dal.ExecutarComandoSQL(sql);
+        }
+
+        public List<EstacaoModel> ListaEstacao()
+        {
+            var lista = new List<EstacaoModel>();
+
+            var sql = $"SELECT ID, DESCRICAO, STATUS, ESTACAO FROM ESTACAO_EST";
+            var dal = new DAL();
+            var dt = dal.RetDataTable(sql);
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                var item = new EstacaoModel
+                {
+                    Id = Convert.ToInt32(dt.Rows[i]["ID"].ToString()),
+                    Descricao = dt.Rows[i]["DESCRICAO"].ToString(),  
+                    Tipo = dt.Rows[i]["STATUS"].ToString()
+                };
+
+                lista.Add(item);
+            }
+
+            return lista;
+        }
+    }
+}
